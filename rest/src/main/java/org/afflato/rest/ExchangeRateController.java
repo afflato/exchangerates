5d3c2191897522d5ab69b2ee4af2a6b585d032b2
afflato <paul.sharadchandra@gmail.com>
2026-03-29 01:21:03 +0530

    min replicas = 3

--- rest/src/main/java/org/afflato/rest/ExchangeRateController.java
+++ rest/src/main/java/org/afflato/rest/ExchangeRateController.java
@@ -3,6 +3,7 @@ package org.afflato.rest;
 import lombok.RequiredArgsConstructor;
 import org.afflato.domain.ExchangeRate;
 import org.afflato.domain.ExchangeRateService;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class ExchangeRateController {
+    public static final String USD = "USD";
     private final ExchangeRateService exchangeRateService;
 
     @GetMapping("/latest/{currency}")
     public ExchangeRate getData(@PathVariable String currency) {
+        currency = USD.equals(currency) ? currency : USD;
         return exchangeRateService.getExchangeRate(currency);
     }
+
 }
